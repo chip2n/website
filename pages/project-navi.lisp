8b11d14576b4f59fc3639d92ae1032053234a5bc
chip2n <andreas@arvidsson.io>
2024-09-13 15:22:46 +0200

    Fix typos

--- pages/project-navi.lisp
+++ pages/project-navi.lisp
@@ -25,7 +25,7 @@
         (:span
           "There's many static site generators available - why make a new one? The main reason is, of course, because it's fun to write one! The distant second reason is that I wanted to have full control over the outputted HTML. I knew that I would need to run arbitrary code during the build process, so any generator that didn't allow for this (or makes you jump through hoops to do so) was out the window. I also knew that I wanted to write the HTML using "
           (:a :href "https://en.wikipedia.org/wiki/S-expression" "S-expressions")
-          ". I find them strictly superior to the tag-soup that is regular HTML. They are easy to generate through code and are very quick to modify using structural editing. What's left is mostly toy-projects, so why not add another toy-project to the pile?")
+          ". I find them strictly superior to the tag-soup that is regular HTML. They are easy to generate through code and are very quick to modify using structural editing. What's left is mostly toy projects, so why not add another toy project to the pile?")
         (:h2 "Implementation")
         (:span
           "Navi is implemented in Common Lisp. It uses Spinneret to render the S-expressions to HTML, and ")
